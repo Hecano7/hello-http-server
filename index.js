bc6286a3d1fa7dd858653d14e39907f2179d1c02
Hecan7 <7hecano@gmail.com>
2020-04-28 13:23:43 -0700

    hello

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code here
+const express = require('express');
+const port =  process.env.PORT || 8080;
+const app = express();
+
+app.get('/',(req,res) => {
+            res.status(200).send('Hello World')
+        });
+
+app.listen(port , function(error){
+    if (error) {
+        console.log('something went wrong', error)
+    }else{
+        console.log('Server is listening on port ' + port )    
+    }
+})
